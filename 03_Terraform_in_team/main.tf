81aa561ac391dffffd06f4d5e423bc468445f0a2
Hussnain Tariq <hussnaintariq151@gmail.com>
2026-01-08 12:27:31 -0800

    Terraform with team

---
+++ 03_Terraform_in_team/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+
+
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = var.bucket_name
+
+  // only for testing purpose not for production
+
+  force_destroy = true
+
+}
+
+# Enable versioning to revision state file history
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server side encryption
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block all public access
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# aws dynamodb for state locking
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
